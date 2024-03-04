1bf913d9a7a3ad8d47efcf8e455b2e49ec92c307
NeoMatlala <neomatlala70@gmail.com>
2024-03-04 22:23:02 +0200

    FE: Messages

--- GFA.Client.FE/pages/contact/index.vue
+++ GFA.Client.FE/pages/contact/index.vue
@@ -5,31 +5,83 @@
             <h1 class="text-center text-4xl md:text-5xl font-medium">Let's Work Together</h1>
         </div>
 
-        <form class="max-w-3xl mx-auto">
+        <div v-if="showSuccess" class="max-w-3xl mx-auto  font-medium text-center p-4 mb-10 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50" role="alert">
+            {{ successMessage }}
+        </div>
+
+        <form class="max-w-3xl mx-auto" @submit.prevent="SendMessage">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-7 mb-7">
                 <div>
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Jon" required />
+                    <input type="text" v-model="message.name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Jon" required />
                 </div>
                 <div>
                     <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                    <input type="text" id="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="0745859658" required />
+                    <input type="text" v-model="message.phoneNumber" id="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="0745859658" required />
                 </div>
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
-                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+                    <input type="email" v-model="message.email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
                 </div>
                 <div>
                     <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subject</label>
-                    <input type="text" id="subject" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Hourly Rate" required />
+                    <input type="text" v-model="message.subject" id="subject" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Hourly Rate" required />
                 </div>
             </div>
             <div class="mb-5">
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a message..."></textarea>
+                <textarea id="message" rows="4" v-model="message.messageBody" class="block whitespace-pre-wrap p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a message..."></textarea>
             </div>
             
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Send message</button>
         </form>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default{
+    data() {
+        return{
+            message: {
+                name: '',
+                phoneNumber: '',
+                email: '',
+                subject: '',
+                messageBody: ''
+            },
+            showSuccess: false,
+            successMessage: '',
+        }
+    },
+    mounted(){
+        this.showSuccess = false
+    },
+    methods: {
+        async SendMessage() {
+            try {
+                const response = await axios.post("https://localhost:7049/api/Message/create-message", this.message)
+                
+                console.log(response.data)
+
+                if (response.data.success) {
+                    this.showSuccess = true
+
+                    this.successMessage = response.data.message
+
+                    this.message.name = ''
+                    this.message.phoneNumber = ''
+                    this.message.email = ''
+                    this.message.subject = ''
+                    this.message.messageBody = ''
+                }
+
+                
+            } catch (error) {
+                console.log("Error sending message", error)
+            }
+        }
+    }
+}
+</script>
--- GFA.Dashboard.FE/pages/messages/index.vue
+++ GFA.Dashboard.FE/pages/messages/index.vue
@@ -2,7 +2,73 @@
     <div class="w-full">
         <div class="mt-5 max-w-[95%] mx-auto">
             <h1 class="text-2xl mb-10 text-slate-800 font-medium">Messages</h1>
-        </div>
 
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Subject
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Email
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Phone Number
+                            </th>
+                            <th scope="col" class="px-6 py-3"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b" :class="{'font-medium bg-blue-50 text-blue-600': !message.isRead}" v-for="message in messages">
+                            <th scope="row" class="px-6 py-4 font-medium ">
+                                {{ message.subject }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ message.name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ message.email }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ message.phoneNumber }}
+                            </td>
+                            <td class="px-6 py-4 flex justify-end items-center gap-5">
+                                <NuxtLink :to="`/messages/view/${message.messageId}`" class="hover:underline text-blue-600">View</NuxtLink>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default{
+    data() {
+        return{
+            messages: []
+        }
+    },
+    mounted() {
+        this.getMessages()
+    },
+    methods: {
+        async getMessages() {
+            try {
+                const response = await axios.get("https://localhost:7049/api/Message/get-all-messages")
+                
+                this.messages = response.data.reverse()
+            } catch (error) {
+                console.log("Error getting messages", error)
+            }
+        }
+    }
+}
+</script>
