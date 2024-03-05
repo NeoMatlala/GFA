6cdad8ec422e77bac2759f5a939923f2d18bd832
NeoMatlala <neomatlala70@gmail.com>
2024-03-05 16:55:34 +0200

    FE: update, delete and message sent dialogs

--- GFA.Dashboard.FE/pages/contacts/update/[id].vue
+++ GFA.Dashboard.FE/pages/contacts/update/[id].vue
@@ -19,6 +19,22 @@
                 <NuxtLink to="/contacts" class="text-slate-400 bg-slate-50 border border-slate-300 hover:bg-slate-200 focus:ring-4 focus:outline-none rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Cancel</NuxtLink>
             </div>
         </form>
+
+        <!-- success modal -->
+        <div id="popup-modal" data-modal-backdrop="static" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-md max-h-full">
+                <div class="relative bg-white rounded-lg shadow">
+                    <div class="p-4 md:p-5 text-center">
+                        <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+                        <h3 class="mb-5 text-lg font-normal text-gray-5000">Successfully updated</h3>
+                        
+                        <button id="closeButton" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Ok, thanks</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,6 +42,8 @@
 import axios from 'axios'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { onMounted } from 'vue'
+import { Modal } from 'flowbite'
 
 export default{
     data() {
@@ -38,6 +56,19 @@ export default{
     },
     setup() {
         const id = ref(useRoute().params.id)
+
+        onMounted(async () => {
+            const $modalElement = document.querySelector('#popup-modal');
+            const $closeButton = document.querySelector('#closeButton');
+            const modalOptions = {
+                backdrop: 'static',
+                backdropClasses: 'bg-gray-900 bg-opacity-50 fixed inset-0 z-40'
+            }
+            if ($modalElement) {
+                const modal = new Modal($modalElement, modalOptions);
+                 $closeButton.addEventListener('click', () => modal.hide());
+            }
+        })
 
         return {
             id
@@ -57,11 +88,30 @@ export default{
             }
         },
         async handleUpdate() {
+            const $closeButton = document.querySelector('#closeButton');
+            const modalOptions = {
+                backdrop: 'static',
+                backdropClasses: 'bg-gray-900 bg-opacity-50 fixed inset-0 z-40'
+            }
+
             try {
                 const response = await axios.put(`https://localhost:7049/api/Socials/update-link/${this.id}`, this.detail)
                 console.log(response.data)
+
+                const $modalElement = document.querySelector('#popup-modal');
+
                 if (response.data.success) {
-                    this.$router.replace('/contacts');
+                    const modal = new Modal($modalElement, modalOptions);
+                    modal.toggle()
+                }
+
+                if ($modalElement) {
+                    const modal = new Modal($modalElement, modalOptions);
+                    modal.toggle();
+                    $closeButton.addEventListener('click', () => {
+                        modal.hide()
+                        this.$router.replace('/contacts');
+                    });
                 }
             } catch (error) {
                 console.log("Error creating contact details", error)
